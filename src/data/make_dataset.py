be7674b817cd8ea0995bf5601e5f7365d357bd47
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-04-24 23:45:46 -0400

    Convert 'amount' column to numerical

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,9 +6,7 @@ df_accounts = pd.read_csv(input_filepath)
 df_accounts = df_accounts.rename(columns={' Amount ': 'Amount'})
 df_accounts.columns = df_accounts.columns.str.lower()
 df_accounts['amount']  = pd.to_numeric(df_accounts['amount'].str.replace('[^\-.0-9]', '', regex=True), errors='coerce')
-
-df_accounts[df_accounts['amount'] < 0]
-
+df_accounts.drop(df_accounts[df_accounts['amount'] < 0].index, inplace=True)
 
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (data/p0-raw) into
@@ -19,6 +17,19 @@ def main(input_filepath, output_filepath):
     df_accounts = df_accounts.rename(columns={' Amount ': 'Amount'})
     df_accounts.columns = df_accounts.columns.str.lower()
     df_accounts['amount']  = pd.to_numeric(df_accounts['amount'].str.replace('[^\-.0-9]', '', regex=True), errors='coerce')
+    """
+    Delete negative valued columns
+
+    In 2021-2022 there was only one negative row, here are its details:
+
+               amount            ministry            category               payment detail                           recipient statutory additional detail
+    8731 -259956709.0  Ministry Of Health  Statutory Payments  Ontario Government Pharmacy  Less: Distribution And Cash Sales   No Value          No Value
+
+    It was deleted, not sure what was going on with that. Either it was mislabeled, so we need to make it positive. Or something else was going on.
+
+    """
+    df_accounts.drop(df_accounts[df_accounts['amount'] < 0].index, inplace=True)
+
 
 
 if __name__ == '__main__':
