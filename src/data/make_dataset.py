7e36341c6ea1b8feebd5811e8fa6955855e2713b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-04-24 23:48:47 -0400

    Ran black on it

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,28 @@
 ## Part 0: Intialization
 import pandas as pd
 
-input_filepath = 'data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv'
+input_filepath = "data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv"
 df_accounts = pd.read_csv(input_filepath)
-df_accounts = df_accounts.rename(columns={' Amount ': 'Amount'})
+df_accounts = df_accounts.rename(columns={" Amount ": "Amount"})
 df_accounts.columns = df_accounts.columns.str.lower()
-df_accounts['amount']  = pd.to_numeric(df_accounts['amount'].str.replace('[^\-.0-9]', '', regex=True), errors='coerce')
-df_accounts.drop(df_accounts[df_accounts['amount'] < 0].index, inplace=True)
-df_accounts.dropna(axis='rows', inplace=True)
+df_accounts["amount"] = pd.to_numeric(
+    df_accounts["amount"].str.replace("[^\-.0-9]", "", regex=True), errors="coerce"
+)
+df_accounts.drop(df_accounts[df_accounts["amount"] < 0].index, inplace=True)
+df_accounts.dropna(axis="rows", inplace=True)
+
 
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (data/p0-raw) into
-        cleaned data ready to be analyzed (saved in data/p1-interim).
+    """Runs data processing scripts to turn raw data from (data/p0-raw) into
+    cleaned data ready to be analyzed (saved in data/p1-interim).
     """
-    input_filepath = 'data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv'
+    input_filepath = "data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv"
     df_accounts = pd.read_csv(input_filepath)
-    df_accounts = df_accounts.rename(columns={' Amount ': 'Amount'})
+    df_accounts = df_accounts.rename(columns={" Amount ": "Amount"})
     df_accounts.columns = df_accounts.columns.str.lower()
-    df_accounts['amount']  = pd.to_numeric(df_accounts['amount'].str.replace('[^\-.0-9]', '', regex=True), errors='coerce')
+    df_accounts["amount"] = pd.to_numeric(
+        df_accounts["amount"].str.replace("[^\-.0-9]", "", regex=True), errors="coerce"
+    )
     """
     Delete negative valued columns
 
@@ -29,10 +34,11 @@ def main(input_filepath, output_filepath):
     It was deleted, not sure what was going on with that. Either it was mislabeled, so we need to make it positive. Or something else was going on.
 
     """
-    df_accounts.drop(df_accounts[df_accounts['amount'] < 0].index, inplace=True)
-    df_accounts.dropna(axis='rows', inplace=True)
+    df_accounts.drop(df_accounts[df_accounts["amount"] < 0].index, inplace=True)
+    df_accounts.dropna(axis="rows", inplace=True)
 
-if __name__ == '__main__':
-    input_filepath = 'data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv'
-    output_filepath = 'data/p1-interim/public-accounts.csv'
+
+if __name__ == "__main__":
+    input_filepath = "data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv"
+    output_filepath = "data/p1-interim/public-accounts.csv"
     main(input_filepath, output_filepath)
