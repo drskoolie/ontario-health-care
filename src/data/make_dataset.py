d294212dea7baae8059634e2f906b0d2b0090759
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-04-24 23:19:55 -0400

    Set up filepath locations for data input

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """ Runs data processing scripts to turn raw data from (data/p0-raw) into
+        cleaned data ready to be analyzed (saved in data/p1-interim).
     """
 
 if __name__ == '__main__':
-    main(1,2)
+    input_filepath = 'data/p0-raw/public-accounts/detailed-2021-22_2022-09-22.csv'
+    output_filepath = 'data/p1-interim/public-data.csv'
+    main(input_filepath, output_filepath)
